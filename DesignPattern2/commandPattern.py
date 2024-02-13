d2624266ef19b22dd06674b5992a7ece4be36edf
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-02-13 19:00:37 +0530

    half completed command pattern'

--- DesignPattern2/commandPattern.py
+++ DesignPattern2/commandPattern.py
@@ -1,11 +1,12 @@
 #---------- The Command Pattern ----------------------
 import sys
 
+# Reciever Object
+
 class Window:
     def exit(self):
         sys.exit(0)
 
-#-- Reciever Object--------
 class Document:
     def __init__(self,filename):
         self.filename = filename
@@ -15,4 +16,60 @@ class Document:
         with open(self.filename,"w") as file:
             file.write(self.contents)
 
-#-- Invoker Object-----------
+# Invoker Object
+            
+class ToolbarButton:
+    def __init__(self,name,iconname):
+        self.name = name
+        self.iconname = iconname
+
+    def click(self):
+        self.command.execute()
+
+
+class MenuItem:
+    def __init__(self,menu_name,menuitem_name):
+        self.menu = menu_name
+        self.item = menuitem_name
+
+    def click(self):
+        self.command.execute()
+
+class KeyboardShortcut:
+    def __init__(self,key,modifier):
+        self.key = key
+        self.modifier = modifier
+
+    def keypress(self):
+        self.command.execute()
+
+
+# Command Object
+        
+class SaveCommand:
+    def __init__(self,document):
+        self.document = document
+
+    def execute(self):
+        self.document.save()
+
+class ExitCommand:
+    def __init__(self,window):
+        self.window = window
+    
+    def execute(self):
+        self.window.exit()
+
+
+
+window = Window()
+document = Document("document.txt")
+save = SaveCommand(document)
+exit = ExitCommand(window)
+
+save_button = ToolbarButton("save","save.png")
+save_button.command = save
+save_keystroke = KeyboardShortcut("s","ctrl")
+save_keystroke.command = save
+exit_menu = MenuItem("File","Exit")
+exit_menu.command = exit
