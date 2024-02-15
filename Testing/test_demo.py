2d939a3e3c71876a2282ac3b1edbaf80e6e08d4e
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-02-15 15:07:16 +0530

    ignoring broken test

--- Testing/test_demo.py
+++ Testing/test_demo.py
@@ -1,7 +1,24 @@
 import unittest
+import sys
 
-class CheckNumbers(unittest.TestCase):
-    def test_int_float(self):
-        self.assertEqual(1,1.0)
+class SkipTests(unittest.TestCase):
 
-        
+    @unittest.expectedFailure
+    def test_fails(self):
+        self.assertEqual(True,False)
+
+    @unittest.skip("This test is useless.")
+    def test_skip(self):
+        self.assertEqual(True,False)
+
+    @unittest.skipIf(sys.version_info.minor==11,"broken on 3.4")
+    def test_skipIf(self):
+        self.assertEqual(True,False)
+
+    @unittest.skipUnless(sys.platform.startswith("linux"),"broken unless on linux")
+    def test_skipUnless(self):
+        self.assertEqual(True,False)
+
+
+if __name__ == "__main__":
+    unittest.main()
