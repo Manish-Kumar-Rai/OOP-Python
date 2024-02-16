93191bace305822caeef0aef7d6a8fe9a9a334ee
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-02-16 23:11:26 +0530

    completed mock object

--- Testing/expensiveOjects.py
+++ Testing/expensiveOjects.py
@@ -6,15 +6,16 @@ from unittest.mock import patch
 import pytest
 import datetime
 
-
 @pytest.fixture
 def tracker():
     return FlightStatusTracker()
+
 
 def test_mock_method(tracker):
     tracker.redis.set = Mock()
     with pytest.raises(ValueError) as ex:
         tracker.change_status("AC101","lost")
+
     assert ex.value.args[0] == "LOST is not a valid status."
     assert tracker.redis.set.call_count == 0
 
@@ -25,6 +26,6 @@ def test_patch(tracker):
         dt.now.return_value = fake_now
         tracker.change_status("AC101","on-time")
     dt.now.assert_called_once_with()
-    tracker.redis.set(
+    tracker.redis.set.assert_called_once_with(
         "flightno:AC101","2024-02-16T00:00:00 | ON-TIME"
     )
