fbe59db3516795adbf76f93ea537350458cfd780
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-02-16 15:21:06 +0530

    Completed Fixture

--- Testing/test_with_pytest.py
+++ Testing/test_with_pytest.py
@@ -37,30 +37,50 @@ import os.path, shutil, tempfile
 #     assert "b" in dir_contents
 
 #--------------- scope of a fixture ---------------
-import socket
-import subprocess
-import time
+# import socket
+# import subprocess
+# import time
 
-@pytest.fixture(scope="session")
-def echoserver():
-    print("Loading Server....")
-    p = subprocess.Popen(["python3","server.py"])
-    time.sleep(1)
-    yield p
-    p.terminate()
+# @pytest.fixture(scope="session")
+# def echoserver():
+#     print("Loading Server....")
+#     p = subprocess.Popen(["python3","server.py"])
+#     time.sleep(1)
+#     yield p
+#     p.terminate()
 
-@pytest.fixture
-def clientsocket(request):
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect(("localhost",1028))
-    yield s
-    s.close()
+# @pytest.fixture
+# def clientsocket(request):
+#     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#     s.connect(("localhost",1028))
+#     yield s
+#     s.close()
 
 
-def test_echo1(echoserver,clientsocket):
-    clientsocket.send(b"abc")
-    assert clientsocket.recv(3) == b"abc"
+# def test_echo1(echoserver,clientsocket):
+#     clientsocket.send(b"abc")
+#     assert clientsocket.recv(3) == b"abc"
 
-def test_echo(echoserver,clientsocket):
-    clientsocket.send(b"def")
-    assert clientsocket.recv(3) == b"def"
+# def test_echo(echoserver,clientsocket):
+#     clientsocket.send(b"def")
+#     assert clientsocket.recv(3) == b"def"
+
+
+# ------ Skipping test with pytest------------
+import sys
+
+# def test_simple_skip():
+#     if sys.platform != "fakeos":
+#         pytest.skip("Test works only on fakeOS")
+#     fakeos.do_something_fake()
+#     assert fakeos.did_not_happen
+
+# @pytest.mark.skipif("sys.version_info <= (3,0)")
+# def test_python3():
+#     assert b"hello".decode() == "hello"
+
+name= "manishrai"
+
+@pytest.mark.xfail("name == 'manish'")
+def test_fails():
+    assert 3 == 3
