03fca24f0aa7b76e4bf024dfb51f34c015966b6b
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-02-19 15:38:37 +0530

    learning Thread Class

--- Concurrency/threads.py
+++ Concurrency/threads.py
@@ -1,1 +1,20 @@
 #------------------------ Threads ----------------------
+
+from threading import Thread
+
+class InputReader(Thread):
+    def run(self):
+        self.line_of_text = input()
+
+print("Enter some text and then enter.")
+thread = InputReader()
+thread.run()
+
+count = result = 1
+
+while thread.is_alive():
+    result = count * count
+    count += 1
+
+print("calculate squares upto {0} * {0} = {1}".format(count,result))
+print("While you type '{}'.".format(thread.line_of_text))
