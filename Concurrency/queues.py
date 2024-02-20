f88b2d00d88ed3218ad355cb2cc4e9538dc4fe57
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-02-20 20:54:01 +0530

    Completed multiprocessing Queues

--- Concurrency/queues.py
+++ Concurrency/queues.py
@@ -1,8 +1,40 @@
 #-----------------------   Queues ------------------------
 
+def search(paths,query_q,result_q):
+    lines = []
+    for path in paths:
+        lines.extend(l.strip() for l in path.open())
 
-arr = [1,2,3]
-arr1 = [4,8,9]
+    query = query_q.get()
+    while query:
+        result_q.put([l for l in lines if query in l])
+        query = query_q.get()
 
-arr.extend(arr1)
-print(arr)
+
+if __name__ == "__main__":
+    from multiprocessing import Process, Queue, cpu_count
+    from path import Path
+
+    cpus = cpu_count()
+    pathnames = [f for f in Path(".").iterdir() if f.isfile()]
+    paths = [pathnames[i::cpus] for i in range(cpus)]
+    query_queues = [Queue() for _ in range(cpus)]
+    result_queues = Queue()
+    search_procs = [
+        Process(target=search, args=(p,q,result_queues))
+        for p, q in zip(paths,query_queues)
+    ]
+
+    for proc in search_procs:
+        proc.start()
+
+    for q in query_queues:
+        q.put("def")
+        q.put(None)
+
+    for i in range(cpus):
+        for match in result_queues.get():
+            print(match)
+
+    for proc in search_procs:
+        proc.join()
