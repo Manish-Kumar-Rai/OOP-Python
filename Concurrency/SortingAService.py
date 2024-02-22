f77126fb217a49df4012e90caf42efa6a8025f77
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-02-22 23:12:36 +0530

    learning Sorting as a Service

---
+++ Concurrency/SortingAService.py
@@ -0,0 +1,42 @@
+#-------------- Sorting as a Service -----------------
+
+import asyncio
+import json
+from concurrent.futures import ProcessPoolExecutor
+
+def sort_in_process(data):
+    nums = json.loads(data.decode())
+    curr = 1
+    while curr < len(nums):
+        if nums[curr] >= nums[curr-1]:
+            curr += 1
+        else:
+            nums[curr] , nums[curr-1] = nums[curr-1] , nums[curr]
+            if curr > 1:
+                curr -= 1
+
+    return json.dumps(nums).encode()
+
+async def sort_request(reader,writer):
+    print("Recieved Connection")
+    length = await reader.read(8)
+    data = await reader.readexactly(int.from_bytes(length,"big"))
+    result = await asyncio.get_event_loop().run_in_executor(
+        None,sort_in_process,data
+    )
+
+    print("Sorted List")
+    writer.write(result)
+    writer.close()
+    print("Connection Closed")
+
+
+loop = asyncio.get_event_loop()
+loop.set_default_executor(ProcessPoolExecutor())
+server = loop.run_until_complete(sort_request,"127.0.0.1",2015)
+print("Sort Servicing running")
+
+loop.run_forever()
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.closed()
